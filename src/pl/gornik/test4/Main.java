29b0359093ccbf4abb79092c7ace3692e665e2b5
lkoin <lkoin.kontakt@gmail.com>
2023-12-15 08:02:51 +0100

    alalallalal2

--- src/pl/gornik/test4/Main.java
+++ src/pl/gornik/test4/Main.java
@@ -1,7 +1,21 @@
 package pl.gornik.test4;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        payment payment;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj kwotę do zaplaty: ");
+        double amount = scanner.nextDouble();
+        System.out.println("Jaki rodzaj platnosci");
+        System.out.println("1. Platnosc gotowka");
+        System.out.println("2. Platnosc karta");
+        System.out.println("3. Platnosc blik");
+        int wybor = scanner.nextInt();
+        switch (wybor) {
+        case 1 -> {
 
-    }
+        }
+    }   }
 }
