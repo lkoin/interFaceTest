44a72e086a4cb6558b89f7bb323be9349df20141
lkoin <lkoin.kontakt@gmail.com>
2023-12-15 07:50:23 +0100

    alalallalal2

--- src/pl/gornik/test4/BlikPayment.java
+++ src/pl/gornik/test4/BlikPayment.java
@@ -1,8 +1,14 @@
 package pl.gornik.test4;
 
+import java.util.Scanner;
+
 public class BlikPayment implements payment{
     @Override
     public void makePayment(double amount) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Platnosc blik");
+        int blik = scanner.nextInt();
+        System.out.println("Kod blik: " + blik);
+        System.out.println("Pobrano z konta: " + amount + " zl");
     }
 }
--- src/pl/gornik/test4/CardPayment.java
+++ src/pl/gornik/test4/CardPayment.java
@@ -1,8 +1,14 @@
 package pl.gornik.test4;
 
+import java.util.Scanner;
+
 public class CardPayment implements payment{
     @Override
     public void makePayment(double amount) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Platnosc kartą");
+        int nrKarty = scanner.nextInt();
+        System.out.println("Nr karty " + nrKarty);
+        System.out.println("Pobrano z karty: " + amount + " zl");
     }
 }
